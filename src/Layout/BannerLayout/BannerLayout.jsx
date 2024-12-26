6fcf52cb566c607d3b404f638d8a38fb8c7e729f
yoginandal <yogidesign1996@gmail.com>
2024-12-26 14:33:30 +0530

    new commit

--- src/Layout/BannerLayout/BannerLayout.jsx
+++ src/Layout/BannerLayout/BannerLayout.jsx
@@ -3,13 +3,16 @@ const Banner = () => {
     <div className="flex flex-col justify-center items-center py-20 px-5 text-center">
       {/* Text Section Above Image */}
       <h1 className="text-5xl lg:text-7xl font-bold text-pgold mb-4 tracking-wide">
-        Step into a world of Health Care
+        Step into Wellness
       </h1>
       <p className="text-xl lg:text-2xl text-gray-500 italic mb-8 max-w-3xl">
-        Embark on a journey of exceptional quality, where
-        <span className="text-lgold font-semibold">health</span> meets
-        <span className="text-lgold font-semibold">elegance</span>. Discover
-        personalized solutions tailored just for you.
+        Discover personalized solutions designed to transform your&nbsp;
+        <span className="text-lgold font-semibold">health</span> and &nbsp;
+        <span className="text-lgold font-semibold">lifestyle</span>. From
+        expert-crafted{" "}
+        <span className="text-lgold font-semibold">nutrition</span> plans to
+        holistic care, experience the perfect blend of health and elegance
+        tailored just for you.
       </p>
 
       {/* The banner with centered image */}
@@ -23,8 +26,9 @@ const Banner = () => {
 
       {/* Text Section Below Image */}
       <p className="text-xl lg:text-2xl text-gray-600 mt-8 font-light">
-        <span className="text-lgold font-semibold">Elevate your lifestyle</span>
-        with tailored nutrition plans, expertly crafted just for you.
+        Start your journey to a &nbsp;
+        <span className="text-lgold font-semibold">healthier,</span>
+        &nbsp;elevated you.
       </p>
     </div>
   );
